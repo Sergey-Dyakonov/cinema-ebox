f8c79bfea07308b799658843d4dafe5a5f70c876
Serhii Diakonov <s.dyakonov@knubisoft.com>
2024-03-23 16:17:56 +0200

    Models moved to dto package

--- src/main/java/io/swagger/dto/FullSession.java
+++ src/main/java/io/swagger/dto/FullSession.java
@@ -1,18 +1,12 @@
-package io.swagger.model;
+package io.swagger.dto;
 
 import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import io.swagger.model.Cinema;
-import io.swagger.model.Movie;
-import io.swagger.model.Place;
-import io.swagger.model.Session;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.validation.annotation.Validated;
 import javax.validation.Valid;
-import javax.validation.constraints.*;
 
 /**
  * FullSession
--- src/main/java/io/swagger/dto/Place.java
+++ src/main/java/io/swagger/dto/Place.java
@@ -1,4 +1,4 @@
-package io.swagger.model;
+package io.swagger.dto;
 
 import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -6,8 +6,6 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.validation.annotation.Validated;
-import javax.validation.Valid;
-import javax.validation.constraints.*;
 
 /**
  * Place
